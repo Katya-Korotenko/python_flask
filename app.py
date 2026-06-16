64727310b7c871f3ba8589616aa38a7bbfaaa144
Katya-Korotenko <korotenkok99943@gmail.com>
2026-06-16 21:00:09 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+
+@app.route('/user/<path:username>')
+def show_user(username):
+    return f'Hello, {username}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
